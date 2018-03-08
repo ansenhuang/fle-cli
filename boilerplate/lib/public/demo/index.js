bbb933d676a3fe694add2ea7f78dcff903512db2
huangancheng <huangancheng@corp.netease.com>
2018-03-08 22:36:34 +0800

    fix: boilerplate

--- boilerplate/lib/public/demo/index.js
+++ boilerplate/lib/public/demo/index.js
@@ -1,13 +1,7 @@
 import module from '@'
-import css from './main.css'
+import './style.css'
 
 // run module
 module()
 
-document.getElementById('root').innerHTML = [
-  '<h1>Hello, fle-cli.</h1>',
-  /* when open css-modules */
-  `<p class="${css.title}">This is module css.</p>`
-  /* when close css-modules */
-  // '<p class="title">This is global css.</p>'
-].join('')
+document.getElementById('root').innerHTML = '<h1>Hello, fle-cli.</h1>'
--- boilerplate/module/src/component/index.js
+++ boilerplate/module/src/component/index.js
@@ -1,1 +1,5 @@
+import module from './module'
+
+module()
+
 document.getElementById('root').innerHTML = '<h1>Hello, fle-cli.</h1>'
