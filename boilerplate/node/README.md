5ae037c29e14f541827ac976f93be426dd295dc6
ansenhuang <ancheng1992@126.com>
2018-05-20 01:34:39 +0800

    feat: add node boilerplate

---
+++ boilerplate/node/README.md
@@ -0,0 +1,9 @@
+## Build Setup
+
+``` bash
+# build src files
+fle lib -w
+fle lib
+```
+
+For detailed explanation, consult the [docs for fle-cli](https://www.npmjs.com/package/fle-cli).
---
+++ boilerplate/node/src/index.js
@@ -0,0 +1,19 @@
+function formatDate (date = new Date(), fmt = 'yyyy-MM-dd hh:mm:ss') {
+  let o = {
+    MM: date.getMonth() + 1,
+    dd: date.getDate(),
+    hh: date.getHours(),
+    mm: date.getMinutes(),
+    ss: date.getSeconds()
+  }
+
+  fmt = fmt.replace('yyyy', date.getFullYear())
+
+  Object.keys(o).forEach(k => {
+    fmt = fmt.replace(k, o[k] > 9 ? o[k] : '0' + o[k])
+  })
+
+  return fmt
+}
+
+console.log(formatDate())
