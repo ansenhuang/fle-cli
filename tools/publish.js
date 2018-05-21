89178b7e7a757e300590ea286fed2c4a9591ba8f
ansenhuang <ancheng1992@126.com>
2018-05-21 17:18:41 +0800

    chore: publish tools

--- tools/publish.js
+++ tools/publish.js
@@ -1,14 +1,6 @@
 var spawn = require('cross-spawn');
-var args = process.argv.slice(2);
+var version = process.argv[2] || 'patch';
 
-var level = 'patch';
-
-if (args.indexOf('minor') !== -1) {
-  level = 'minor';
-} else if (args.indexOf('major') !== -1) {
-  level = 'major';
-}
-
-spawn.sync('npm', ['version', level], { stdio: 'inherit' });
+spawn.sync('npm', ['version', version], { stdio: 'inherit' });
 spawn.sync('npm', ['publish'], { stdio: 'inherit' });
 spawn.sync('git', ['push'], { stdio: 'inherit' });
