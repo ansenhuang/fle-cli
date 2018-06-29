6386ea38e8fb849eb68bfcbe7c75d86bf6c55337
ansenhuang <ancheng1992@126.com>
2018-06-29 15:52:49 +0800

    optimize: dev host

--- build/webpack/plugin.js
+++ build/webpack/plugin.js
@@ -3,6 +3,7 @@ var path = require('path');
 var webpack = require('webpack');
 var notifier = require('node-notifier');
 var internalIp = require('internal-ip');
+var color = require('@fle/color');
 
 var config = require('./config');
 var { resolve } = require('./utils');
@@ -84,8 +85,8 @@ exports.friendlyErrors = () => {
   return new FriendlyErrorsPlugin({
     compilationSuccessInfo: {
       messages: [
-        `Local    ->  http://${config.fle.host}:${config.fle.port}/`,
-        `Network  ->  http://${localIP}:${config.fle.port}/`
+        'Local    ->  ' + color.cyan(`http://${config.fle.host}:${config.fle.port}/`),
+        'Network  ->  ' + color.cyan(`http://${localIP}:${config.fle.port}/`)
       ]
     },
     onErrors: (severity, errors) => {
